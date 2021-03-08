c86bae924d19ca0eb37ced30082d0df417567710
eloraschoerverth <elora666sdg@gmail.com>
2021-03-08 19:43:07 +0100

    changes

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,13 +1,13 @@
 const config = require('./src/config');
 
 module.exports = {
+  pathPrefix: '/',
   siteMetadata: {
     title: 'Elora Schoerverth',
-    description:
-      'Brittany Chiang is a software engineer specializing in building (and occasionally designing) exceptional websites, applications, and everything in between.',
-    siteUrl: 'https://brittanychiang.com', // No trailing slash allowed!
+    description: 'Elora is a CS Graduate Student at TU Berlin.',
+    siteUrl: 'https://eloraschoerverth.github.io', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
-    twitterUsername: '@bchiang7',
+    twitterUsername: '@',
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
--- src/components/sections/featured.js
+++ src/components/sections/featured.js
@@ -247,93 +247,11 @@ const StyledProject = styled.div`
 `;
 
 const Featured = () => {
-  const data = useStaticQuery(graphql`
-    query {
-      featured: allMarkdownRemark(
-        filter: { fileAbsolutePath: { regex: "/featured/" } }
-        sort: { fields: [frontmatter___date], order: DESC }
-      ) {
-        edges {
-          node {
-            frontmatter {
-              title
-              cover {
-                childImageSharp {
-                  fluid(maxWidth: 700, traceSVG: { color: "#64ffda" }) {
-                    ...GatsbyImageSharpFluid_withWebp_tracedSVG
-                  }
-                }
-              }
-              tech
-              github
-              external
-            }
-            html
-          }
-        }
-      }
-    }
-  `);
 
-  const featuredProjects = data.featured.edges.filter(({ node }) => node);
-
-  const revealTitle = useRef(null);
-  const revealProjects = useRef([]);
-  useEffect(() => {
-    sr.reveal(revealTitle.current, srConfig());
-    revealProjects.current.forEach((ref, i) => sr.reveal(ref, srConfig(i * 100)));
-  }, []);
 
   return (
     <section id="projects">
-      <h2 className="numbered-heading" ref={revealTitle}>
-        Some Things I’ve Built
-      </h2>
 
-      <div>
-        {featuredProjects &&
-          featuredProjects.map(({ node }, i) => {
-            const { frontmatter, html } = node;
-            const { external, title, tech, github, cover } = frontmatter;
-
-            return (
-              <StyledProject key={i} ref={el => (revealProjects.current[i] = el)}>
-                <div className="project-content">
-                  <p className="project-overline">Featured Project</p>
-                  <h3 className="project-title">{title}</h3>
-                  <div className="project-description" dangerouslySetInnerHTML={{ __html: html }} />
-
-                  {tech.length && (
-                    <ul className="project-tech-list">
-                      {tech.map((tech, i) => (
-                        <li key={i}>{tech}</li>
-                      ))}
-                    </ul>
-                  )}
-
-                  <div className="project-links">
-                    {github && (
-                      <a href={github} aria-label="GitHub Link">
-                        <Icon name="GitHub" />
-                      </a>
-                    )}
-                    {external && (
-                      <a href={external} aria-label="External Link" className="external">
-                        <Icon name="External" />
-                      </a>
-                    )}
-                  </div>
-                </div>
-
-                <div className="project-image">
-                  <a href={external ? external : github ? github : '#'}>
-                    <Img fluid={cover.childImageSharp.fluid} alt={title} className="img" />
-                  </a>
-                </div>
-              </StyledProject>
-            );
-          })}
-      </div>
     </section>
   );
 };
